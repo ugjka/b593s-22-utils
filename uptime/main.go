40ee8f00560b38fdf1e09a7a42e18a92d6d4d4a4
ugjka <esesmu@protonmail.com>
2018-12-20 17:36:21 +0200

    add uptime

---
+++ uptime/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"syscall"
+	"time"
+
+	"github.com/hako/durafmt"
+)
+
+func main() {
+	sys := &syscall.Sysinfo_t{}
+	err := syscall.Sysinfo(sys)
+	errExit(err)
+	dur := time.Duration(sys.Uptime) * time.Second
+	humandur := durafmt.Parse(dur)
+	fmt.Printf("up %s\n", humandur)
+}
+
+func errExit(err error) {
+	if err == nil {
+		return
+	}
+	fmt.Fprintf(os.Stderr, "Error: %v", err)
+	os.Exit(1)
+}
