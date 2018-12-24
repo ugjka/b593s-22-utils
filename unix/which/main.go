58b65a25a47a7fe32d1683cdc979fe2d2c693d77
ugjka <esesmu@protonmail.com>
2018-12-24 12:25:58 +0200

    better implementation

--- unix/which/main.go
+++ unix/which/main.go
@@ -2,21 +2,66 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
-	"os/exec"
+	"strings"
 )
 
 func main() {
-	a := os.Args
-	if len(a) < 2 {
-		fmt.Fprintf(os.Stderr, "No name given!\n")
-		return
+	all := flag.Bool("a", false, "print all matches in PATH")
+	sym := flag.Bool("s", false, "resolve symlink")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		errExit(fmt.Errorf("no argument given"))
 	}
-	path, err := exec.LookPath(a[1])
+	path := os.Getenv("PATH")
+	if path == "" {
+		errExit(fmt.Errorf("no path variable found"))
+	}
+	dirs := strings.Split(path, ":")
+	for _, v := range dirs {
+		v = v + "/" + args[0]
+		if isExist(v) && isExec(v) {
+			if *sym {
+				v = resolve(v)
+			}
+			fmt.Println(v)
+			if !*all {
+				break
+			}
+		}
+	}
+}
+
+func errExit(err error) {
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "%s does not exist!\n", a[1])
-		return
+		log.Fatal(err)
 	}
-	fmt.Println(path)
+}
+
+func isExist(path string) bool {
+	_, err := os.Lstat(path)
+	if err == nil {
+		return true
+	}
+	return false
+}
+
+func isExec(path string) bool {
+	info, err := os.Lstat(path)
+	if err != nil {
+		return false
+	}
+	return info.Mode()&0111 != 0
+}
+
+func resolve(path string) string {
+	to, err := os.Readlink(path)
+	if err != nil {
+		return path
+	}
+	return fmt.Sprintf("%s -> %s", path, to)
 }
